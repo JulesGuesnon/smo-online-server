aec9e9c6e5e8ff8f3cd9a3245c2ed71f06217bb8
Jack Baron <jackmbaron@gmail.com>
2022-08-05 03:22:23 +0100

    impl better tracing fmt

--- src/main.rs
+++ src/main.rs
@@ -37,14 +37,19 @@ use std::str::FromStr;
 use std::sync::Arc;
 use std::time::Duration;
 
+use clap::Parser;
+use color_eyre::Result;
+use once_cell::sync::Lazy;
 use owo_colors::OwoColorize;
 use server::Server;
 use settings::Settings;
-use tokio::io::{self, AsyncWriteExt};
+use tokio::io::AsyncWriteExt;
 use tokio::net::TcpListener;
 use tokio::time::sleep;
 use tracing::{debug, info};
-use tracing_subscriber::EnvFilter;
+use tracing_error::ErrorLayer;
+use tracing_subscriber::prelude::*;
+use tracing_subscriber::{fmt, EnvFilter};
 
 mod commands;
 mod packet;
@@ -53,20 +58,56 @@ mod players;
 mod server;
 mod settings;
 
-#[tokio::main]
-async fn main() -> io::Result<()> {
-    let subscriber = tracing_subscriber::fmt()
-        .with_env_filter(EnvFilter::from_str("info").unwrap())
-        .finish();
+static VERSION: Lazy<String> = Lazy::new(|| {
+    let mut version = format!("v{}", env!("CARGO_PKG_VERSION"));
+    if let Some(hash) = option_env!("GIT_SHORT_HASH") {
+        use std::fmt::Write as _;
+        let _ = write!(version, " ({})", hash);
+    }
 
-    tracing::subscriber::set_global_default(subscriber).unwrap();
+    version
+});
+
+#[derive(Debug, Parser)]
+#[clap(version = &VERSION[..], about)]
+struct Args {
+    /// Verbosity level
+    #[clap(short, long, parse(from_occurrences))]
+    verbose: u8,
+}
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    color_eyre::install()?;
+    let args = Args::parse();
+
+    let pkg_name = env!("TRACING_FMT");
+    let filter = match args.verbose {
+        #[cfg(debug_assertions)]
+        0 | 1 | 2 => format!("{}=debug", pkg_name),
+
+        #[cfg(not(debug_assertions))]
+        0 => format!("{}=info,{}=info", pkg_name),
+        #[cfg(not(debug_assertions))]
+        1 | 2 => format!("{}=debug", pkg_name),
+
+        3 => format!("{}=trace", pkg_name),
+        _ => "trace".into(),
+    };
+
+    let filter = EnvFilter::new(filter);
+    let fmt = fmt::layer().with_target(args.verbose >= 2);
+
+    tracing_subscriber::registry()
+        .with(filter)
+        .with(fmt)
+        .with(ErrorLayer::default())
+        .init();
 
     let settings = Settings::load().await;
-
     let server = Arc::new(Server::new(settings));
 
     let _ = server.load_shines().await;
-
     let bind_address = SocketAddr::from_str(&format!(
         "{}:{}",
         server.settings.read().await.server.address,
